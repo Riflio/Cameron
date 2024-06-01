63df12fe21eabd708cd9d54a52790b87bf55f4e8
Riflio <2me.pavelk@gmail.com>
2024-06-01 23:57:33 +0300

    fix(build): Исправляем путь копирования библиотек

--- BUILD_APP.sh
+++ BUILD_APP.sh
@@ -4,12 +4,12 @@ cd /APP/build-Cameron
 rm -rf *
 /opt/QtDev/install_LinAmd64/bin/qmake /APP/Cameron.pro
 make -j10 VERBOSE=1
-cp /opt/QtDev/install_LinAmd64/libQt6Core.so ./
-cp /opt/QtDev/install_LinAmd64/libQt6Core.so.6 ./
-cp /opt/QtDev/install_LinAmd64/libQt6Core.so.6.8.0 ./
-cp /opt/QtDev/install_LinAmd64/libQt6Network.so ./
-cp /opt/QtDev/install_LinAmd64/libQt6Network.so.6 ./
-cp /opt/QtDev/install_LinAmd64/libQt6Network.so.6.8.0 ./
+cp /opt/QtDev/install_LinAmd64/lib/libQt6Core.so ./
+cp /opt/QtDev/install_LinAmd64/lib/libQt6Core.so.6 ./
+cp /opt/QtDev/install_LinAmd64/lib/libQt6Core.so.6.8.0 ./
+cp /opt/QtDev/install_LinAmd64/lib/libQt6Network.so ./
+cp /opt/QtDev/install_LinAmd64/lib/libQt6Network.so.6 ./
+cp /opt/QtDev/install_LinAmd64/lib/libQt6Network.so.6.8.0 ./
 cp /usr/lib/x86_64-linux-gnu/libcrypto.so.3 ./
 # Plugins
 mkdir plugins-dist && cd plugins-dist
